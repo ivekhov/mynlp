da9846f244620613ff0ec85bf11a468b6fe08777
Ivan.Vekhov <Iv@n)27011985>
2020-06-04 17:57:56 +0300

    added preproc examples

---
+++ useful_code_external/preprocessing/bengfort/sent_parsing.py
@@ -0,0 +1,15 @@
+# example from book
+
+from nltk import sent_tokenize
+
+def sents(paragraph):
+    for sentence in sent_tokenize(paragraph):
+        yield sentence
+
+def parse(textfile):
+    with open(textfile, 'r') as f:
+        text = f.read()
+        return list(sents(text))
+
+print(parse("zen.txt"))
+print(parse("rhyme.txt"))
